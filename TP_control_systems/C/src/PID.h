dcb36dc5b0621ba44dde3508cc3c64b12eb3353e
Samuel <bumann.samuel@gmail.com>
2022-01-13 12:01:54 +0100

    deleted some files and added STEP files of ruler holder

--- TP_control_systems/C/src/PID.h
+++
@@ -1,7 +0,0 @@
-#ifndef PID_h
-#define PID_h
-
-int initialize(char *);
-void calc(double [], double []);
-
-#endif /* PID_h */
--- TP_control_systems/C/testController/Build/Intermediates.noindex/testController.build/Debug/testController.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/philippe/Desktop/TP_control_systems/C/testController/testController/main.c \
-  /Users/philippe/Desktop/TP_control_systems/C/src/PID.h
